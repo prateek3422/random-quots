1b2f169a2e4dcc010d42eeb498a570d5a3a4d8cb
prateek3422 <ps0431299@gmail.com>
2025-03-20 14:59:24 +0530

    Add Random Quote Generator web application with features and setup instructions

---
+++ README.md
@@ -0,0 +1,44 @@
+# Random Quote Generator
+
+A simple web application that displays random inspirational quotes with a beautiful background. Users can get new quotes, copy them to clipboard, or share them directly on Twitter.
+
+![Random Quote Generator Screenshot](image-1.png)
+
+## Features
+
+- Fetches random quotes from an external API
+- Displays quotes with author attribution
+- Aesthetically pleasing UI with background from Unsplash
+- Responsive design that works on all devices
+- Functionality to get new quotes with a button click
+- Copy quotes to clipboard
+- Share quotes directly to Twitter
+
+## Technologies Used
+
+- HTML5
+- CSS3 with Tailwind CSS
+- JavaScript (ES6+)
+- Font Awesome icons
+- External APIs:
+  - FreeAPI for quotes
+  - Unsplash for background images
+
+## Installation and Setup
+
+1. Clone the repository:
+
+   ```
+   git clone https://github.com/yourusername/get-random-quots.git
+   cd get-random-quots
+   ```
+
+2. No build process is required as this is a simple HTML/JS application.
+
+3. Open `index.html` in your browser to run the application locally.
+
+## Usage
+
+- Click the "New quote" button to fetch and display a new random quote
+- Click the "Copy quote" button to copy the current quote to your clipboard
+- Click the "Tweet quote" button to share the quote on Twitter
---
+++ script.js
@@ -0,0 +1,50 @@
+const bg = document.getElementById("quote-box");
+const quote = document.getElementById("quote");
+const author = document.getElementById("author");
+const newButton = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+const copyButton = document.getElementById("copy-quote");
+const twitterButton = document.getElementById("tweet-quote");
+
+// get random background image from unsplash api
+
+bg.style.backgroundImage = `url('https://images.unsplash.com/photo-1482192596544-9eb780fc7f66?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')`;
+
+bg.style.backgroundSize = "cover";
+bg.style.backgroundPosition = "center";
+bg.style.backgroundRepeat = "no-repeat";
+
+// get random quote from freeapi
+
+const getQuote = async () => {
+  const response = await fetch(
+    "https://api.freeapi.app/api/v1/public/quotes/quote/random"
+  );
+  const data = await response.json();
+
+  quote.innerHTML = data?.data?.content;
+  author.innerHTML = data?.data?.author;
+};
+
+// call functions to get random background image and quote
+
+// getbg();
+getQuote();
+
+// new quote fetch
+newButton.addEventListener("click", () => {
+  getQuote();
+});
+
+// copy to clipboard
+copyButton.addEventListener("click", () => {
+  const text = `${quote.innerHTML}`;
+  navigator.clipboard.writeText(text);
+});
+
+// tweet quote
+
+twitterButton.addEventListener("click", () => {
+  const twitterUrl = `https://twitter.com/intent/tweet/?text=${quote.innerHTML} - ${author.innerHTML}`;
+  window.open(twitterUrl, "_blank");
+});
